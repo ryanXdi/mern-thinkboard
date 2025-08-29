e3328508bbdffcda710c97273971fda880c4e750
ryanXdi <minswanpyae7@gmail.com>
2025-08-29 15:12:37 +0700

    rate limiter updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, '20 s'), // 10 requests per 20 seconds
+    limiter: Ratelimit.slidingWindow(100, '60 s'), // 10 requests per 20 seconds
 });
 
 export default ratelimit;
